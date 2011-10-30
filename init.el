099586681556cd1926391f391137668a1615751c
gc <gc@gcbuntu.(none)>
2011-10-30 14:28:51 +0900

    haml yaml

--- init.el
+++ init.el
@@ -6,28 +6,35 @@
 
 ;; haml-mode
 (require 'haml-mode)
+(add-to-list 'auto-mode-alist '("\\.haml$" . haml-mode))
 
 ;; yaml-mode
 (require 'yaml-mode)
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 
+;; backup files
+(setq make-backup-files t)
+(setq backup-directory-alist
+  (cons (cons "\\.*$" (expand-file-name "~/.emacs.d/backup"))
+    backup-directory-alist))
+
 ;; php-mode
 ;;(load-library "php-mode")
 ;;(require 'php-mode)
-(autoload 'php-mode "php-mode" t)
+;; (autoload 'php-mode "php-mode" t)
 
 ;; mmm-mode
 
-(require 'mmm-mode)
-(setq mmm-global-mode 'maybe)
-(set-face-background 'mmm-default-submode-face "snow2")
-(mmm-add-mode-ext-class nil "\\.php?\\'" 'html-php)
-(mmm-add-classes
-'((html-php
-:submode php-mode
-:front "<\\?\\(php\\)?"
-:back "\\?>")))
-(add-to-list 'auto-mode-alist '("\\.php?\\'" . xml-mode))
+;; (require 'mmm-mode)
+;; (setq mmm-global-mode 'maybe)
+;; (set-face-background 'mmm-default-submode-face "snow2")
+;; (mmm-add-mode-ext-class nil "\\.php?\\'" 'html-php)
+;; (mmm-add-classes
+;; '((html-php
+;; :submode php-mode
+;; :front "<\\?\\(php\\)?"
+;; :back "\\?>")))
+;; (add-to-list 'auto-mode-alist '("\\.php?\\'" . xml-mode))
 
 ;; set utf8
 ;; (set-language-environment "Japanese")
@@ -48,4 +55,4 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "unknown" :family "ricty")))))
