3912cf6c28bf9f71a4ab65379e7e816fedbe36f2
yuuki.inoue <w@gc6.jp>
2014-08-10 18:54:34 +0900

    dismiss

--- init.el
+++ init.el
@@ -150,6 +150,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t)
  '(matlab-indent-level 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
