2c5b74848e759ac5a3e9b811b23beefca8831cca
yuuki.inoue <w@gc6.jp>
2014-08-10 18:55:54 +0900

    dismiss menubar

--- init.el
+++ init.el
@@ -151,7 +151,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(inhibit-startup-screen t)
- '(matlab-indent-level 2))
+ '(matlab-indent-level 2)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
