f9ee280ab0a92dfb8c55d90cd692403765de813f
yuuki.inoue <yuuki.inoue@link-u.co.jp>
2017-01-10 18:33:22 +0900

    use autocomplete

--- init/20_go_mode.el
+++ init/20_go_mode.el
@@ -1,9 +1,11 @@
+(defun auto-complete-for-go ()
+  (auto-complete-mode 1))
 ;; go-mode
 (add-hook 'go-mode-hook (lambda ()
-                          (company-mode)
+                                        ; (company-mode)
+                                        ; (set (make-local-variable 'company-backends) '(company-go))
+                          (auto-complete-for-go)
                           (setq gofmt-command "goimports")
-                          (set (make-local-variable 'company-backends) '(company-go))
-                          ; (add-to-list 'company-backends 'company-go)
                           (add-hook 'before-save-hook 'gofmt-before-save)
                           (set (make-local-variable 'compile-command) "go build -v && go test -v && go vet")
                           (local-set-key (kbd "M-.") 'godef-jump)
@@ -11,3 +13,8 @@
                           (flycheck-mode)
                           )
           )
+;(custom-set-variables
+; '(ac-go-expand-arguments-into-snippets nil)) ; auto-complete
+
+(with-eval-after-load 'go-mode
+   (require 'go-autocomplete))
